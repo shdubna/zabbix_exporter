e2480575fcd2c886ae528b8f87d0265eab19ebdf
smirnov <smirnovav@monqlab.com>
2021-09-05 17:02:19 +0300

    First release

--- zabbix/zabbix.go
+++ zabbix/zabbix.go
@@ -1,190 +1,185 @@
 package zabbix
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/akomic/go-zabbix-proto/client"
-	"github.com/prometheus/client_golang/prometheus"
-	"log"
-	"strings"
-	"unicode"
+        "encoding/json"
+        "fmt"
+        "github.com/akomic/go-zabbix-proto/client"
+        "github.com/prometheus/client_golang/prometheus"
+        "log"
+        "strings"
+        "unicode"
 )
 
 const (
-	namespace = "zabbix" // For Prometheus metrics.
+        namespace = "zabbix"
 )
 
-// Zabbix.
+// Zabbix structure.
 type Zabbix struct {
-	Client *client.Client
+        Client *client.Client
 }
 
-// Zabbix constructor
+// NewZabbix constructor
 func NewZabbix(host string, port int) (p *Zabbix) {
-	p = &Zabbix{
-		Client: client.NewClient(host, port),
-	}
-	return
+        p = &Zabbix{
+                Client: client.NewClient(host, port),
+        }
+        return
 }
 
-// Get server metrics from Zabbix.
+// GetMetrics from Zabbix.
 func (zabbix *Zabbix) GetMetrics() (response *ZabbixResponse, err error) {
-	packet := zabbix.NewStatsPacket(`zabbix.stats`)
+        packet := zabbix.NewStatsPacket(`zabbix.stats`)
 
-	var res []byte
-	res, err = zabbix.Client.Send(packet)
-	if err != nil {
-		return
-	}
+        var res []byte
+        res, err = zabbix.Client.Send(packet)
+        if err != nil {
+                return
+        }
 
-	response, err = NewZabbixResponse(res)
-	if err != nil {
-		return
-	}
+        response, err = NewZabbixResponse(res)
+        if err != nil {
+                return
+        }
 
-	if response.Response != `success` {
-		err = fmt.Errorf("Error sending heartbeat: %s", response.Response)
-	}
-	return
+        if response.Response != `success` {
+                err = fmt.Errorf("Error sending heartbeat: %s", response.Response)
+        }
+        return
 }
 
-
-// Stats Packet.
+// StatsPacket structure.
 type StatsPacket struct {
-	Request string `json:"request"`
+        Request string `json:"request"`
 }
 
-//stats Packet constructor.
+// NewStatsPAcket constructor.
 func (zabbix *Zabbix) NewStatsPacket(request string) *client.Packet {
-	ap := &StatsPacket{Request: request}
-	jsonData, _ := json.Marshal(ap)
-	packet := &client.Packet{Request: request, Data: jsonData}
-	return packet
+        ap := &StatsPacket{Request: request}
+        jsonData, _ := json.Marshal(ap)
+        packet := &client.Packet{Request: request, Data: jsonData}
+        return packet
 }
 
-// ZabbixResponse class.
+// ZabbixResponse structure.
 type ZabbixResponse struct {
-	Response string  `json:"response"`
-	Data     map[string]interface{}
+        Response string `json:"response"`
+        Data     map[string]interface{}
 }
 
-// ZabbixResponse class constructor.
+// NewZabbixResponse constructor.
 func NewZabbixResponse(data []uint8) (r *ZabbixResponse, err error) {
-	if len(data) < 13 {
-		err = fmt.Errorf("NewZabbixResponse Input data to short")
-		return
-	}
-	jsonData := data[13:]
-	r = &ZabbixResponse{Response: string(jsonData)}
-	err = json.Unmarshal(jsonData, r)
-	if err != nil {
-		err = fmt.Errorf("Error decoding response: %v", err)
-		if e, ok := err.(*json.SyntaxError); ok {
-			err = fmt.Errorf("%s ; Syntax error at byte offset %d", err, e.Offset)
-		}
-		return
-	}
-	return
+        if len(data) < 13 {
+                err = fmt.Errorf("NewZabbixResponse Input data to short")
+                return
+        }
+        jsonData := data[13:]
+        r = &ZabbixResponse{Response: string(jsonData)}
+        err = json.Unmarshal(jsonData, r)
+        if err != nil {
+                err = fmt.Errorf("Error decoding response: %v", err)
+                if e, ok := err.(*json.SyntaxError); ok {
+                        err = fmt.Errorf("%s ; Syntax error at byte offset %d", err, e.Offset)
+                }
+                return
+        }
+        return
 }
 
-
+// Describe describe metrics
 func (e *Zabbix) Describe(ch chan<- *prometheus.Desc) {
-	// We cannot know in advance what metrics the exporter will generate
-	// from zabbix. So we use the poor man's describe method: Run a collect
-	// and send the descriptors of all the collected metrics.
 
-	metricCh := make(chan prometheus.Metric)
-	doneCh := make(chan struct{})
+        metricCh := make(chan prometheus.Metric)
+        doneCh := make(chan struct{})
 
-	go func() {
-		for m := range metricCh {
-			ch <- m.Desc()
-		}
-		close(doneCh)
-	}()
+        go func() {
+                for m := range metricCh {
+                        ch <- m.Desc()
+                }
+                close(doneCh)
+        }()
 
-	e.Collect(metricCh)
-	close(metricCh)
-	<-doneCh
+        e.Collect(metricCh)
+        close(metricCh)
+        <-doneCh
 }
 
+// Collect metrics
 func (e *Zabbix) Collect(ch chan<- prometheus.Metric) {
-	upValue := 1
+        upValue := 1
 
-	if err := e.collect(ch); err != nil {
-		log.Printf("Error scraping zabbix: %s", err)
-		upValue = 0
-	}
+        if err := e.collect(ch); err != nil {
+                log.Printf("Error scraping zabbix: %s", err)
+                upValue = 0
+        }
 
-	ch <- prometheus.MustNewConstMetric(
-		prometheus.NewDesc(
-			prometheus.BuildFQName(namespace, "", "up"),
-			"Was the last query of Zabbix successful.",
-			nil, nil,
-		),
-		prometheus.GaugeValue, float64(upValue),
-	)
+        ch <- prometheus.MustNewConstMetric(
+                prometheus.NewDesc(
+                        prometheus.BuildFQName(namespace, "", "up"),
+                        "Was the last query of Zabbix successful.",
+                        nil, nil,
+                ),
+                prometheus.GaugeValue, float64(upValue),
+        )
 }
 
 func (e *Zabbix) collect(ch chan<- prometheus.Metric) error {
-	metrics, err := e.GetMetrics()
-	if err != nil {
-		return fmt.Errorf("Error scraping zabbix: %v", err)
-	}
-    getMetricRecursive(metrics.Data, ch, "")
-	return nil
+        metrics, err := e.GetMetrics()
+        if err != nil {
+                return fmt.Errorf("Error scraping zabbix: %v", err)
+        }
+        getMetricRecursive(metrics.Data, ch, "")
+        return nil
 }
 
 func getMetricRecursive(metrics map[string]interface{}, ch chan<- prometheus.Metric, prefix string) {
-	for key, value := range metrics {
-		name := prefix+key
-		switch value.(type) {
-		case  float64:
-			newMetric := prometheus.NewGaugeVec(prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      metricName(name),
-				//Help:      "Number of " + name + " currently processed",
-			}, []string{}).WithLabelValues()
-			newMetric.Set(value.(float64))
-			newMetric.Collect(ch)
-		case string:
-			if key == "version" {
-                newMetric := prometheus.NewGaugeVec(prometheus.GaugeOpts{
-					Namespace: namespace,
-					Name:      metricName("info"),
-					Help:      "Info abount instance",
-					
-				}, []string{"version"}).WithLabelValues(value.(string))
-				newMetric.Set(1)
-				newMetric.Collect(ch)
-		    }
-		case map[string]interface{}:
-			//log.Printf("other %v",value)
-			getMetricRecursive(value.(map[string]interface{}), ch, name+"_")
-		}
-	}
+        for key, value := range metrics {
+                name := prefix + key
+                switch value.(type) {
+                case float64:
+                        newMetric := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+                                Namespace: namespace,
+                                Name:      metricName(name),
+                                //Help:      "Number of " + name + " currently processed",
+                        }, []string{}).WithLabelValues()
+                        newMetric.Set(value.(float64))
+                        newMetric.Collect(ch)
+                case string:
+                        if key == "version" {
+                                newMetric := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+                                        Namespace: namespace,
+                                        Name:      metricName("info"),
+                                        Help:      "Info abount instance",
+                                }, []string{"version"}).WithLabelValues(value.(string))
+                                newMetric.Set(1)
+                                newMetric.Collect(ch)
+                        }
+                case map[string]interface{}:
+                        //log.Printf("other %v",value)
+                        getMetricRecursive(value.(map[string]interface{}), ch, name+"_")
+                }
+        }
 }
 
-
 func metricName(in string) string {
-	out := toSnake(in)
-	r := strings.NewReplacer(".","_"," ","","-","")
-	return r.Replace(out)
+        out := toSnake(in)
+        r := strings.NewReplacer(".", "_", " ", "", "-", "")
+        return r.Replace(out)
 }
 
 func toSnake(in string) string {
-	runes := []rune(in)
-	length := len(runes)
+        runes := []rune(in)
+        length := len(runes)
 
-	var out []rune
-	for i := 0; i < length; i++ {
-		if i > 0 && unicode.IsUpper(runes[i]) && ((i+1 < length && unicode.IsLower(runes[i+1])) || unicode.IsLower(runes[i-1])) {
-			out = append(out, '_')
-		}
-		out = append(out, unicode.ToLower(runes[i]))
-	}
+        var out []rune
+        for i := 0; i < length; i++ {
+                if i > 0 && unicode.IsUpper(runes[i]) && ((i+1 < length && unicode.IsLower(runes[i+1])) || unicode.IsLower(runes[i-1])) {
+                        out = append(out, '_')
+                }
+                out = append(out, unicode.ToLower(runes[i]))
+        }
 
-	return string(out)
+        return string(out)
 }
 
 // check interface
--- zabbix_exporter.go
+++ zabbix_exporter.go
@@ -1,51 +1,49 @@
 package main
 
 import (
-	"flag"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"log"
-	"net/http"
-	"strconv"
-	"zabbix_exporter/zabbix"
-	"fmt"
-	"os"
+        "flag"
+        "fmt"
+        "github.com/prometheus/client_golang/prometheus"
+        "github.com/prometheus/client_golang/prometheus/promhttp"
+        "log"
+        "net/http"
+        "os"
+        "strconv"
+        "zabbix_exporter/zabbix"
 )
 
 var gitTag string
 
 var (
-	listeningAddress    = flag.String("telemetry.address", ":9051", "Address on which to expose metrics.")
-	metricsEndpoint     = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
-	zabbixAddr          = flag.String("zabbix_addr", "127.0.0.1", "Zabbix server addr")
-	zabbixPort          = flag.Int("zabbix_port", 10051, "Zabbix server port")
-	version             = flag.Bool("version", false, "Show version number and quit")
+        listeningAddress = flag.String("telemetry.address", ":9051", "Address on which to expose metrics.")
+        metricsEndpoint  = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
+        zabbixAddr       = flag.String("zabbix_addr", "127.0.0.1", "Zabbix server addr")
+        zabbixPort       = flag.Int("zabbix_port", 10051, "Zabbix server port")
+        version          = flag.Bool("version", false, "Show version number and quit")
 )
 
-
-
 func main() {
-	flag.Parse()
-	if *version {
-		fmt.Println(gitTag)
-		os.Exit(0)
-	  }
-	log.Printf("Scraping %s:%s", *zabbixAddr, strconv.Itoa(*zabbixPort))
+        flag.Parse()
+        if *version {
+                fmt.Println(gitTag)
+                os.Exit(0)
+        }
+        log.Printf("Scraping %s:%s", *zabbixAddr, strconv.Itoa(*zabbixPort))
 
-	registerer := prometheus.DefaultRegisterer
-	gatherer := prometheus.DefaultGatherer
-	e := zabbix.NewZabbix(*zabbixAddr, *zabbixPort)
-	registerer.MustRegister(e)
+        registerer := prometheus.DefaultRegisterer
+        gatherer := prometheus.DefaultGatherer
+        e := zabbix.NewZabbix(*zabbixAddr, *zabbixPort)
+        registerer.MustRegister(e)
 
-	http.Handle(*metricsEndpoint, promhttp.HandlerFor(gatherer, promhttp.HandlerOpts{}))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`<html>
-			<head><title>Zabbix Exporter</title></head>
-			<body>
-			<h1>Zabbix Exporter</h1>
-			<p><a href="` + *metricsEndpoint + `">Metrics</a></p>
-			</body>
-			</html>`))
-	})
-	log.Fatal(http.ListenAndServe(*listeningAddress, nil))
+        http.Handle(*metricsEndpoint, promhttp.HandlerFor(gatherer, promhttp.HandlerOpts{}))
+        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+                w.Write([]byte(`<html>
+                        <head><title>Zabbix Exporter</title></head>
+                        <body>
+                        <h1>Zabbix Exporter</h1>
+                        <p><a href="` + *metricsEndpoint + `">Metrics</a></p>
+                        </body>
+                        </html>`))
+        })
+        log.Fatal(http.ListenAndServe(*listeningAddress, nil))
 }
